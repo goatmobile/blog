3fd131a6000864228fc7b90a34c1560d4fedd5f8
goatmobile <80506289+goatmobile@users.noreply.github.com>
2022-12-30 19:43:15 -0800

    cors

--- static/analytics.js
+++ static/analytics.js
@@ -1,12 +1,18 @@
-const url = 'https://docs.google.com/forms/d/e/1FAIpQLSe90WgD5p1Ufhn4Wzp-9OZ918WAT9JE8KGSijFIuYmeVbLntA/formResponse'
-const ua = window.navigator ? window.navigator.userAgent : 'none'
+(function () {
+  if (window.location.host.includes("localhwost")) {
+    return;
+  }
+  const url =
+    "https://docs.google.com/forms/d/e/1FAIpQLSe90WgD5p1Ufhn4Wzp-9OZ918WAT9JE8KGSijFIuYmeVbLntA/formResponse";
+  const ua = window.navigator ? window.navigator.userAgent : "none";
 
-const body = new FormData();
-body.append('entry.445489075', window.location.pathname);
-body.append('entry.242821687', ua);
+  const body = new FormData();
+  body.append("entry.445489075", window.location.pathname);
+  body.append("entry.242821687", ua);
 
-fetch(url, {
-  method: 'POST',
-  mode: 'no-cors',
-  body: body
-})
+  fetch(url, {
+    method: "POST",
+    mode: "no-cors",
+    body: body,
+  });
+})();
